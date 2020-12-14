69cef547aae079e8fe34b8717eb5a0e6d79036a1
Daniel Santos <neco.daniel@gmail.com>
2020-12-14 19:35:25 -0300

    novas alteracoes voltada para os testes

--- src/test/java/br/com/acert/api/conversor/temperaturas/AppTests.java
+++ src/test/java/br/com/acert/api/conversor/temperaturas/AppTests.java
@@ -1,11 +1,29 @@
 package br.com.acert.api.conversor.temperaturas;
 
+import br.com.acert.api.conversor.temperaturas.util.StringUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
 
 @SpringBootTest
 class AppTests {
     @Test
     void contextLoads() {
+    }
+
+
+    private void toStringEnd(ResultActions response, MediaType mediaType) throws Exception {
+        String result = response.andReturn().getResponse().getContentAsString();
+        String out = "";
+
+        if (mediaType == MediaType.APPLICATION_JSON)
+            out = StringUtil.formatConteudoJSONFrom(result);
+
+        if (mediaType == MediaType.APPLICATION_XML)
+            out = StringUtil.formatConteudoXMLFrom(result);
+
+        System.out.println("#TEST_RESULT: ".concat(out));
+        System.out.println("-------------------------------------------------------------");
     }
 }
